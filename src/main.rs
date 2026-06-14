2361461d659e673406ff12a82534586f7388d0eb
Orbán Levente <orban.levente.laszlo@gmail.com>
2026-06-14 19:32:49 +0200

    rework cli

--- src/main.rs
+++ src/main.rs
@@ -11,76 +11,77 @@ use puncture::{bitreader::BitReader, extraction::Extractor};
 fn main() -> Result<(), io::Error> {
     let args = get_cli_args()?;
 
-    let is_extract = args.get_flag("extract");
-    let _is_archive = args.get_flag("archive");
+    let is_decompress = args.get_flag("decompress");
     let to_stdout = args.get_flag("stdout");
+    let keep = args.get_flag("keep");
 
-    let input = args.get_one::<String>("INPUT");
-    let output = args.get_one::<String>("OUTPUT");
+    let files: Vec<&str> = args
+        .get_many::<String>("FILES")
+        .unwrap_or_default()
+        .map(String::as_str)
+        .collect();
 
-    if is_extract
-        && let Some(input_path) = input
-        && input_path != "-"
-    {
-        let input_stream = Box::new(BufReader::with_capacity(
-            1024 * 1024,
-            File::open(input_path)?,
-        ));
+    let files = if files.is_empty() { vec!["-"] } else { files };
 
-        run_extraction(to_stdout, input, output, input_stream)?;
-    } else {
-        let input_stream = Box::new(BufReader::with_capacity(1024 * 1024, std::io::stdin()));
+    for file in files {
+        if is_decompress {
+            if file == "-" {
+                let input_stream =
+                    Box::new(BufReader::with_capacity(1024 * 1024, std::io::stdin()));
 
-        run_extraction(to_stdout, input, output, input_stream)?;
+                run_extraction(to_stdout, file, input_stream)?;
+            } else {
+                let input_stream =
+                    Box::new(BufReader::with_capacity(1024 * 1024, File::open(file)?));
+
+                run_extraction(to_stdout, file, input_stream)?;
+
+                if !to_stdout && !keep {
+                    std::fs::remove_file(file)?;
+                }
+            }
+        } else {
+            eprintln!("Compression is not yet implemented. Use -d to decompress.");
+            std::process::exit(1);
+        }
     }
 
     Ok(())
 }
 
-fn run_extraction<R: BufRead>(
-    to_stdout: bool,
-    input: Option<&String>,
-    output: Option<&String>,
-    input_stream: Box<R>,
-) -> io::Result<()> {
+fn run_extraction<R: BufRead>(to_stdout: bool, file: &str, input_stream: R) -> io::Result<()> {
     let mut br = BitReader::new(input_stream);
     let mut ext = Extractor::new(&mut br);
 
     ext.process_header()?;
 
-    let mut output_stream = get_output_stream(to_stdout, input, output, &ext)?;
+    let mut output_stream = get_output_stream(to_stdout, file, &ext)?;
 
     ext.deflate(&mut output_stream)
 }
 
 fn get_output_stream(
     print_to_stdout: bool,
-    cli_input: Option<&String>,
-    cli_output: Option<&String>,
+    file: &str,
     extractor: &Extractor<'_, impl BufRead>,
 ) -> io::Result<Box<dyn Write>> {
-    Ok(if print_to_stdout {
+    Ok(if print_to_stdout || file == "-" {
         Box::new(std::io::stdout())
-    } else if let Some(requested_file_name) = cli_output {
-        Box::new(File::create(requested_file_name)?)
     } else if let Some(original_file_name) = extractor.get_file_name() {
         let name = original_file_name
             .clone()
             .into_string()
             .map_err(|_| io::Error::other("Original file name isn't valid UTF8"))?;
+
         Box::new(File::create_new(name)?)
-    } else if let Some(input_path) = cli_input
-        && input_path != "-"
-    {
-        Box::new(File::create_new(
-            Path::new(&input_path)
-                .file_stem()
-                .map(Path::new)
-                .expect("Somehow there was no file name"),
-        )?)
     } else {
-        // If the stdout flag wasn't given, there was no explicit output and no explicit input, we'll use a placeholder
-        Box::new(File::create_new("decompressed.txt")?)
+        let path = Path::new(file);
+        let stem = path
+            .file_stem()
+            .map(Path::new)
+            .expect("Invalid file name provided");
+
+        Box::new(File::create_new(stem)?)
     })
 }
 
@@ -89,38 +90,31 @@ fn get_cli_args() -> io::Result<ArgMatches> {
         .version("0.1.0")
         .about("Compress or uncompress files using the gzip format.")
         .arg(
-            Arg::new("extract")
-                .short('e')
-                .long("extract")
-                .help("Set mode to extract")
-                .action(ArgAction::SetTrue)
-                .conflicts_with("archive"),
-        )
-        .arg(
-            Arg::new("archive")
-                .short('a')
-                .long("archive")
-                .help("Set mode to archive (compress)")
+            Arg::new("decompress")
+                .short('d')
+                .long("decompress")
+                .help("decompress")
                 .action(ArgAction::SetTrue),
-        )
-        .arg(
-            Arg::new("INPUT")
-                .help("The input file to process, defaults to stdin if omitted")
-                .required(false)
-                .index(1),
-        )
-        .arg(
-            Arg::new("OUTPUT")
-                .help("Where to extract the files")
-                .required(false)
-                .index(2),
         )
         .arg(
             Arg::new("stdout")
-                .short('s')
+                .short('c')
                 .long("stdout")
-                .help("Routes output to standard out")
+                .help("write on standard output, keep original files unchanged")
                 .action(ArgAction::SetTrue),
+        )
+        .arg(
+            Arg::new("keep")
+                .short('k')
+                .long("keep")
+                .help("keep (don't delete) input files")
+                .action(ArgAction::SetTrue),
+        )
+        .arg(
+            Arg::new("FILES")
+                .help("FILEs to process. With no FILE, or when FILE is -, read standard input.")
+                .action(ArgAction::Append)
+                .num_args(1..),
         );
 
     if std::env::args().len() == 1 && std::io::stdin().is_terminal() {
